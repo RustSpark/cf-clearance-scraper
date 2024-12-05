b811c8c9fd27db4756ea1e9f3600f2ab17667ebb
RustSpark <75129382+RustSpark@users.noreply.github.com>
2024-12-05 20:31:01 +0800

    init

--- src/log.js
+++ src/log.js
@@ -2,7 +2,7 @@ const pino = require('pino');
 const fs = require('fs');
 
 // 创建一个写入流，指向日志文件
-const logStream = fs.createWriteStream('./app.log', {flags: 'a'});
+const logStream = fs.createWriteStream('/opt/logs/cloudflare.log', {flags: 'a'});
 
 // 创建一个 pino logger 实例，并将输出流定向到文件
 const logger = pino(logStream);
